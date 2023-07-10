0aa44d9477e66781386e7fc33b7840de85476e60
unknown <mahmudul474>
2023-07-10 06:23:42 +0600

    register done

--- src/Pages/Authentication/Register/Register.js
+++ src/Pages/Authentication/Register/Register.js
@@ -43,6 +43,7 @@ export default function Register() {
   const [profileImageUrl, setProfileImageUrl] = useState(null);
   const [nidImageUrl, setNidImageUrl] = useState(null);
 
+  ///profile pic   upload
   const handleProfileImageUpload = event => {
     const file = event.target.files[0];
     const s3 = new AWS.S3();
@@ -65,6 +66,7 @@ export default function Register() {
     });
   };
 
+  ///nit upload 
   const handleNidImageUpload = event => {
     const file = event.target.files[0];
     const s3 = new AWS.S3();
@@ -87,6 +89,7 @@ export default function Register() {
     });
   };
 
+  ////create user
   const handleRegistration = e => {
     e.preventDefault();
     if (password.length < 5) {
@@ -100,7 +103,7 @@ export default function Register() {
     signUpUser(email, password)
       .then(result => {
         const user = result.user;
-        console.log(user);
+        
         //save user database
 
         const userInfo = {
@@ -129,6 +132,8 @@ export default function Register() {
       });
   };
 
+
+  //save  user  database
   const saveData = user => {
     const userInfo = {
       userPhoto: profileImageUrl,
@@ -176,10 +181,12 @@ export default function Register() {
           <div className="sm:flex sm:flex-col    sm:w-full lg:flex-row lg:w-full">
             <div className="lg:w-3/4 sm:w-full px-16">
               <div className="my-10 text-center">
-                <h1 className="text-3xl capitalize  mb-1 font-semibold ">
+                <h1 className="text-3xl capitalize   text-black  mb-1 font-semibold ">
                   SIGN UP
                 </h1>
-                <h3 className="text-lg ">We're happy you're here!</h3>
+                <h3 className="text-lg text-black  ">
+                  We're happy you're here!
+                </h3>
               </div>
 
               <hr />
@@ -189,7 +196,7 @@ export default function Register() {
                   <div className="my-1   w-full">
                     <label
                       for="Fname"
-                      className="block text-left mb-2 text-sm font-medium text-gray-900 "
+                      className="block text-left mb-2 text-sm font-medium  text-black "
                     >
                       First Name
                     </label>
@@ -199,7 +206,7 @@ export default function Register() {
                       type="text"
                       name="Fname"
                       id="Fname"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5    dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5       "
                       placeholder="john "
                       required
                     />
@@ -207,7 +214,7 @@ export default function Register() {
                   <div className="my-1 w-full">
                     <label
                       for="Lname"
-                      className="block text-left mb-2 text-sm font-medium text-gray-900 "
+                      className="block text-left mb-2 text-sm font-medium  text-black "
                     >
                       Last Name
                     </label>
@@ -217,7 +224,7 @@ export default function Register() {
                       onChange={e => setLastName(e.target.value)}
                       value={lastName}
                       id="Lname"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5       dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5          "
                       placeholder="doe"
                       required=""
                     />
@@ -228,7 +235,7 @@ export default function Register() {
                   <div className="my-1 w-full">
                     <label
                       for="email"
-                      className="block text-left mb-2 text-sm font-medium text-gray-900 "
+                      className="block text-left mb-2 text-sm font-medium  text-black "
                     >
                       Your email
                     </label>
@@ -238,7 +245,7 @@ export default function Register() {
                       type="email"
                       name="email"
                       id="email"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5      dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5         "
                       placeholder="example@gmail.com"
                       required
                     />
@@ -246,7 +253,7 @@ export default function Register() {
                   <div className="my-1 w-full">
                     <label
                       for="Phone"
-                      className="block text-left mb-2 text-sm font-medium text-gray-900 "
+                      className="block text-left mb-2 text-sm font-medium  text-black "
                     >
                       Phone Number
                     </label>
@@ -256,7 +263,7 @@ export default function Register() {
                       value={phoneNumber}
                       onChange={e => setPhoneNumber(e.target.value)}
                       id="Phone"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5      dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5         "
                       placeholder="Mobile number"
                       required
                     />
@@ -267,7 +274,7 @@ export default function Register() {
                   <div className="my-1 w-full">
                     <label
                       for="ProfilePic"
-                      className="block text-left mb-2 text-sm font-medium text-gray-900 "
+                      className="block text-left mb-2 text-sm font-medium  text-black "
                     >
                       Your Photo
                     </label>
@@ -277,7 +284,7 @@ export default function Register() {
                       onChange={handleProfileImageUpload}
                       name="profile"
                       placeholder="profile"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5     dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5        "
                       required
                     />
                     {profileImageUrl && (
@@ -290,7 +297,7 @@ export default function Register() {
                   <div className="my-1 w-full">
                     <label
                       for="nidPas"
-                      className="block text-left mb-2 text-sm font-medium text-gray-900  "
+                      className="block text-left mb-2 text-sm font-medium  text-black  "
                     >
                       Nid or Passport
                     </label>
@@ -299,7 +306,7 @@ export default function Register() {
                       id="nidPhoto"
                       onChange={handleNidImageUpload}
                       placeholder="upload nid or passport"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5      "
                       required
                     />
 
@@ -316,30 +323,30 @@ export default function Register() {
                   <div className="my-1 w-full">
                     <label
                       for="business name."
-                      className="block text-left mb-2 text-sm font-medium text-gray-900  "
+                      className="block text-left mb-2 text-sm font-medium  text-black  "
                     >
                       Business Name
                     </label>
                     <input
                       onChange={e => setBusinessName(e.target.value)}
                       type="text"
-                      placeholder="Trade license No."
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5       dark:focus:ring-blue-500  "
+                      placeholder="ABC Business ."
+                      className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5          "
                       required
                     />
                   </div>
                   <div className="my-1 w-full">
                     <label
                       for="business Address"
-                      className="block text-left mb-2 text-sm font-medium text-gray-900 "
+                      className="block text-left mb-2 text-sm font-medium  text-black "
                     >
                       Business Address
                     </label>
                     <input
                       onChange={e => setBusinessAddress(e.target.value)}
                       type="text"
-                      placeholder="business tin number"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5    dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Location"
+                      className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5       "
                       required
                     />
                   </div>
@@ -347,8 +354,8 @@ export default function Register() {
                 <div className="flex justify-between flex-col lg:flex-row">
                   <div className="my-1 w-full">
                     <label
-                      for="Trade license No."
-                      className="block text-left mb-2 text-sm font-medium text-gray-900 "
+                      for="123....0"
+                      className="block text-left mb-2 text-sm font-medium  text-black "
                     >
                       Trade license No.
                     </label>
@@ -356,22 +363,22 @@ export default function Register() {
                       onChange={e => setTradeLC(e.target.value)}
                       type="number"
                       placeholder="Trade license No."
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5      "
                       required
                     />
                   </div>
                   <div className="my-1 w-full">
                     <label
                       for="nidPas"
-                      className="block text-left mb-2 text-sm font-medium text-gray-900 "
+                      className="block text-left mb-2 text-sm font-medium  text-black "
                     >
                       Business Tin Number
                     </label>
                     <input
                       onChange={e => setTinNo(e.target.value)}
                       type="number"
-                      placeholder="business tin number"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="123....0"
+                      className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5      "
                       required
                     />
                   </div>
@@ -380,18 +387,19 @@ export default function Register() {
                 <div className="my-1 w-full">
                   <label
                     for="password"
-                    className="block text-left mb-2 text-sm font-medium text-gray-900 "
+                    className="block text-left mb-2 text-sm font-medium  text-black "
                   >
                     Password
                   </label>
                   <input
                     type="password"
+                    min={6}
                     name="password"
                     id="password"
                     value={password}
                     onChange={e => setPassword(e.target.value)}
                     placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className=" bg-white border border-gray-300  text-black sm:text-sm rounded-lg    block w-full p-2.5      "
                     required
                   />
                 </div>
@@ -403,7 +411,7 @@ export default function Register() {
                         id="remember"
                         aria-describedby="remember"
                         type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                        className="w-4 h-4 border border-gray-300 rounded  bg-white focus:ring-3 focus:ring-primary-300 dark: dark:ring-offset-gray-800"
                         required=""
                       />
                     </div>
@@ -418,18 +426,15 @@ export default function Register() {
                   </div>
                 </div>
 
-                
-                 
-                  <button
-                    type="submit"
-                    className="w-1/3 text-white bg-green-600 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
-                  >
-                    Sign up
-                  </button>
-               
+                <button
+                  type="submit"
+                  className="w-1/3 text-white bg-[#719f18] hover:bg-[#73471b]  cursor-pointer focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
+                >
+                  Sign up
+                </button>
               </form>
             </div>
-            <div className="lg:w-1/4 sm:w-full bg-[#442db9] rounded-md py-16 ">
+            <div className="lg:w-1/4 sm:w-full bg-[#73471b] rounded-md  justify-center  flex flex-col item-center py-16 ">
               <div className="mt-24 text-white my-10">
                 <h1 className="text-3xl font-semibold">
                   ALREADY HAVE AN ACCOUNT?
@@ -440,7 +445,7 @@ export default function Register() {
                 <Link to="/login">
                   <button
                     type="button"
-                    className="flex justify-center items-center max-w-sm w-full bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 focus:outline-none text-white text-xl uppercase shadow-md rounded-lg mx-auto p-2"
+                    className="flex justify-center items-center max-w-sm w-full  bg-[#719f18] focus:outline-none text-white text-xl uppercase shadow-md rounded-lg mx-auto p-2"
                   >
                     Login
                   </button>
