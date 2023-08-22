33cc853fa71586cb038d50c4d69b9a6c511d8ffe
mahmudul474 <mdmahmudulla474@gmail.com>
2023-08-22 15:31:54 +0600

    product page ready

--- src/Pages/ProductPage/Cr.js
+++ src/Pages/ProductPage/Cr.js
@@ -1,53 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Product from "../../Shared/ProductCard/Product";
 
 export default function Cr() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/products/category/cr`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
   return (
     <div>
-
- 
-<div style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}} className='h-[200px] bg-opacity-50 flex flex-col justify-center items-start px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
-     
-    <div>
-        <h1 className='text-2xl capitalize  font-semibold'>Live Actions</h1>
-    </div>
-    <div className="text-sm breadcrumbs">
-  <ul>
-    <li><a>Home</a></li> 
-    <li><a>Documents</a></li> 
-    <li>Add Document</li>
-  </ul>
-</div>
-</div>
-
-
-<div class="bg-white">
-  <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
-
-    <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-      <div class="group relative">
-        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+      <div
+        style={{
+          backgroundImage:
+            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+        }}
+        className="h-[200px] bg-opacity-50 flex flex-col justify-center items-start px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8"
+      >
+        <div>
+          <h1 className="text-2xl capitalize  font-semibold">Live Actions</h1>
         </div>
-        <div class="mt-4 flex justify-between">
-          <div>
-            <h3 class="text-sm text-gray-700">
-              <a href="#">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                Basic Tee
-              </a>
-            </h3>
-            <p class="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p class="text-sm font-medium text-gray-900">$35</p>
+        <div className="text-sm breadcrumbs">
+          <ul>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Documents</a>
+            </li>
+            <li>Add Document</li>
+          </ul>
         </div>
       </div>
 
-      
-    </div>
-  </div>
-</div>
+      <div class="bg-white">
+        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 class="text-2xl font-bold tracking-tight text-gray-900">
+            Customers also purchased
+          </h2>
 
+          <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+            {
+                products?.map(product=><Product data={product} key={product._id}></Product>)
+            }
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/Pages/ProductPage/Po.js
+++ src/Pages/ProductPage/Po.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Product from "../../Shared/ProductCard/Product";
 
-export default function Po() {
+export default function Ga() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/products/category/po`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
   return (
-    <div>Po</div>
-  )
+    <div>
+      <div
+        style={{
+          backgroundImage:
+            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+        }}
+        className="h-[200px] bg-opacity-50 flex flex-col justify-center items-start px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8"
+      >
+        <div>
+          <h1 className="text-2xl capitalize  font-semibold">Live Actions</h1>
+        </div>
+        <div className="text-sm breadcrumbs">
+          <ul>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Documents</a>
+            </li>
+            <li>Add Document</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="bg-white">
+        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 class="text-2xl text-left  font-bold tracking-tight text-gray-900">
+            Customers also purchased
+          </h2>
+
+          <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+            {
+                products?.map(product=><Product data={product} key={product._id}></Product>)
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
