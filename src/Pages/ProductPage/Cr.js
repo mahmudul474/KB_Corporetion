195389e5e7c9d229bc4f294cfbfd4cdf96c31cd7
mahmudul474 <mdmahmudulla474@gmail.com>
2023-09-05 14:46:26 +0600

    done

--- src/Pages/ProductPage/Cr.js
+++ src/Pages/ProductPage/Cr.js
@@ -1,60 +1,57 @@
 import React, { useEffect, useState } from "react";
 import Product from "../../Shared/ProductCard/Product";
  import crBg from "./assates/cr.jpg";
+import ProductBidding from "./singleProductDettails/ProductBidding";
+import UserSpinner from "../../Shared/UserSpinner/UserSpinner";
 
  export default function Cr() {
    const [products, setProducts] = useState([]);
+    const [isloading, setIsloading]=useState(false)
 
    useEffect(() => {
+    setIsloading(true)
      fetch(`${process.env.REACT_APP_API_URL}/products/category/cr`)
        .then(res => res.json())
        .then(data => {
          setProducts(data);
-       });
+     setIsloading(false);
+        });
    }, []);
+
+   if(isloading){
+    return <div role="status flex ">
+        <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <span class="sr-only">Loading...</span>
+    </div>
+    
+   }
+
+ // Get the last index
+ const lastIndex = products.length - 1;
+
+ // Get the last product
+ const lastProduct = products[lastIndex];
+
+ 
+
 
    return (
      <div>
-       <div
-         style={{
-           backgroundImage: `url(${crBg})`
-         }}
-         className="h-[200px]  bg-opacity-50 flex flex-col justify-center items-start px-4 "
-       >
-         <div className="py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-           <div>
-             <h1 className="text-2xl text-white capitalize  font-semibold">
-               Live Actions
-             </h1>
-           </div>
-           <div className="text-sm breadcrumbs text-white">
-             <ul>
-               <li>
-                 <a>Category</a>
-               </li>
-               <li>
-                 <a>CR</a>
-               </li>
-               <li>Product</li>
-             </ul>
-           </div>
-         </div>
-       </div>
+      
 
        <div className="bg-white">
+
+{lastProduct && <ProductBidding data={lastProduct}></ProductBidding>}
+
+
          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
            <h2 className="text-2xl   text-left font-bold tracking-tight text-gray-900">
-             CR (Cold Rolled) Steel Coils and Sheets
+            Recommended CR (Cold Rolled) Steel Coils and Sheets
            </h2>
-           <p className="text-black hidden lg:block text-left">
-             Explore our premium selection of Cold Rolled (CR) steel coils and
-             sheets, renowned for their smooth surface finish and precise
-             dimensions. These high-quality steel products are ideal for
-             applications that demand superior surface quality, tight
-             tolerances, and excellent formability. Whether for manufacturing,
-             construction, or other industries, our CR steel ensures reliability
-             and performance in every project.
-           </p>
+          
 
            <div className="divider w-full my-3 h-[2px] text-black bg-black"></div>
 
